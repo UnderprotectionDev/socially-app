a49a1377a0d72e2d0b7d6c9b17624e8495e3b735
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-04 23:08:50 +0300

    Enhance user interaction by adding 'WhoToFollow' component to the homepage and implementing user follow/unfollow functionality. Removed unused SVG files to clean up the project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/create-post";
+import WhoToFollow from "@/components/who-to-follow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
@@ -6,7 +7,9 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">123</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
